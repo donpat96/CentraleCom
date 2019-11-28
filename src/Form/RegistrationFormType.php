a649aa431736967ff2838c7f4b5ca3ac1495b633
donpat96 <ntsagonganbe.patrickdonatien@myu.universitecentrale.tn>
2019-11-29 00:32:31 +0100

    modif form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -33,11 +33,11 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
