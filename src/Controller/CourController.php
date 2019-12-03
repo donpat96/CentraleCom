482d9ef4b6093f5f12718d4f87cb4ab131d0684f
donpat96 <ntsagonganbe.patrickdonatien@myu.universitecentrale.tn>
2019-12-03 16:32:22 +0100

    cours affichés sur la sidebar et accessible sur detail

--- src/Controller/CourController.php
+++ src/Controller/CourController.php
@@ -2,23 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Cour;
 use App\Repository\CourRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class CourController extends AbstractController
 {
     /**
-     * @Route("/cour", name="cour")
+     * @Route("/cour",name="cour")
      */
-    public function index(CourRepository $courRepo)
+    public function index()
     {
-        $cours = $courRepo->findBy([
-            'id' => $this->getUser()->getId(),
-        ]);
+        $courRepo = $this->getDoctrine()->getRepository(Cour::class);
+        $cours = $courRepo->findAll();
+//        $cours = $courRepo->findBy([
+//            'id' => $this->getUser()->getId(),
+//        ]);
 
         return $this->render('cour/index.html.twig', [
-            'All Course' => $cours,
-        ]);
+            'cours' => $cours,
+        ]);}
+
+    /**
+     * @Route("/detail/{id<\d+>}", name="cour.detail")
+     */
+    public function detailCour(Cour $cour= null) {
+        return $this->render('cour/detail.html.twig',
+            array(
+                'cour' => $cour
+            )
+        );
     }
 }
